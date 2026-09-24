2878d5d16d9edae2964ea6108788a194d14ca451
amorirReverse <123003221+amorirReverse@users.noreply.github.com>
2026-09-24 11:39:27 +0200

    Add initial engine ECU executable

---
+++ ecu/engine/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/**
+ * @brief Entry point of the engine ECU simulation.
+ * 
+ * @return Zero on sucessful execution.
+ */
+
+ int main()
+ {
+    std::cout << "Engine ECU started." << std::endl;
+
+    return 0;
+ }
